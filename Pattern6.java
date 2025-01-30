5072416543092e040b55fba53aba2d21c4d660e0
YasojuJahnavi <yasojujahnavi.jh7@gmail.com>
2025-01-30 19:30:53 +0530

    Add files via upload

---
+++ Pattern6.java
@@ -0,0 +1,23 @@
+public class Pattern6{
+    public static void main(String[] args) {
+        int n = 3; 
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j > i; j--) {
+                System.out.print("  "); 
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println(); 
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print("  ");
+            }
+            for (int k = n; k >= i; k--) {
+                System.out.print("* ");
+            }
+            System.out.println(); 
+        }
+    }
+}
